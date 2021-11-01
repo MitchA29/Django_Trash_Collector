7d4ae02fdd7f62c92f44fa6691988a3ce2c68b74
Mitch Anderson <67846917+MitchA29@users.noreply.github.com>
2021-11-01 16:15:25 -0500

    Satisfying merge conflict.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -8,18 +8,6 @@ from datetime import date
 
 from .models import Employee
 
-# Create your views here.
-
-# TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
-
-
-# def index(request):
-#     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-#     Customer = apps.get_model('customers.Customer')
-#     return render(request, 'employees/index.html')
-
-
-
 @login_required
 def index(request):
     # The following line will get the logged-in user (if there is one) within any view function
@@ -48,3 +36,20 @@ def create(request):
         return HttpResponseRedirect(reverse('employees:index'))
     else:
         return render(request, 'employees/create.html')
+
+@login_required
+def edit_profile(request):
+    logged_in_user = request.user
+    logged_in_employee = Employee.objects.get(user=logged_in_user)
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        zip_from_form = request.POST.get('zip_code')
+        logged_in_employee.name = name_from_form
+        logged_in_employee.zip_code = zip_from_form
+        logged_in_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        context = {
+            'logged_in_employee': logged_in_employee
+        }
+        return render(request, 'employees/edit_profile.html', context)
